a1bd04ae68a57474ae531a39dad0b4ab740b7eee
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2020-12-20 13:50:50 -0800

    Updating routing

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App.js";
 import Theater from "./Theater.js";
 
 function Home (props){
     return (
-        <BrowserRouter>
+        <HashRouter basename='/'>
             <Switch>
                 <Route path='/' exact component={App}/>
                 <Route path='/theater/:beenum' exact component={Theater}/>
                 <Route path='/' render={() => <div>404</div>}/>
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
